b0fa6ae86ef298818bbeeef15da3f7f7650328d2
shijunhong <sjh97@icloud.com>
2022-07-27 13:44:32 +0800

    first commit

---
+++ src/main/java/com/example/missyou/api/v1/BannerController.java
@@ -0,0 +1,16 @@
+package com.example.missyou.api.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/banner")
+public class BannerController {
+
+    @GetMapping("/test")
+    public String test() throws Exception {
+        return "Hello World";
+    }
+}
